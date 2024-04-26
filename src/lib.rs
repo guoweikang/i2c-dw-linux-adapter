533b67abb66ad89d6da1a8f5971c09a9bc1f5efe
guoweikang <guoweikang.kernel@gmail.com>
2024-04-26 17:42:12 +0800

    init: add base init --------
    Signed-off-by: guoweikang <guoweikang.kernel@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,13 @@
 
 mod core_base;
 
-pub(crate) use dw_apb_i2c::{timing::I2cTiming, I2cSpeedMode};
+pub(crate) use dw_apb_i2c::{I2cTiming, I2cSpeedMode};
+pub(crate) use dw_apb_i2c::{I2cDwMasterDriver, I2cDwDriverConfig};
 
-use kernel::device::RawDevice;
 use kernel::{module_platform_driver, of, platform, prelude::*};
-
 use kernel::device::Device;
+use kernel::i2c::*;
+
 //use dw_apb_i2c::{I2cDesignwareDriverConfig};
 
 module_platform_driver! {
@@ -27,6 +28,12 @@ kernel::define_of_id_table! {DW_I2C_OF_MATCH_TABLE, (), [
     (of::DeviceId::Compatible(b"snps,designware-i2c"),None),
 ]}
 
+/*
+struct DwI2cDriver {
+    pure_driver: I2cDesignwareDriver,
+    device: Device
+}
+*/
 struct DwI2cDriver;
 
 impl platform::Driver for DwI2cDriver {
@@ -35,22 +42,20 @@ impl platform::Driver for DwI2cDriver {
 
     fn probe(pdev: &mut platform::Device, _id_info: Option<&Self::IdInfo>) -> Result {
         let irq = pdev.irq_resource(0)?;
-        pdev.pr_info(format_args!("========================== get irq {}", irq));
-
         let reg_base = pdev.ioremap_resource(0)?;
-        pdev.pr_info(format_args!(
-            "========================== ioremap ptr {:p}",
-            reg_base
-        ));
-
         let dev = Device::from_dev(pdev);
         let timing = core_base::i2c_parse_fw_timings(&dev, false);
-        dev.pr_info(format_args!(
-            "========================== timing {:?}",
-            timing
-        ));
-        //let bus_freq = device::
 
+        if i2c_detect_slave_mode(&dev) {
+            pr_err!("unimplement dw slave driver");
+            return Ok(());
+        } 
+        
+        let driver_config = I2cDwDriverConfig::new(irq, timing);
+        let mut i2c_master_driver = I2cDwMasterDriver::new(driver_config, reg_base);
+        i2c_master_driver.config_init()?;
+        i2c_master_driver.setup()?;
+        //let bus_freq = device::
         //let i2c_config  = I2cDesignwareDriverConfig::new();
         Ok(())
     }
