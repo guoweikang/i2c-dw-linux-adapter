90b9dd3443d2d9640fef5480a8071a9963837cc8
guoweikang <guoweikang.kernel@gmail.com>
2024-05-08 16:42:38 +0800

    i2c dw now can register success
    Signed-off-by: guoweikang <guoweikang.kernel@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,19 @@ mod core_base;
 pub(crate) use i2c_common::{I2cTiming, I2cSpeedMode};
 pub(crate) use dw_apb_i2c::{I2cDwMasterDriver, I2cDwDriverConfig};
 
-use kernel::{module_platform_driver, of, platform, prelude::*};
-use kernel::device::Device;
-use kernel::i2c::*;
-
-//use dw_apb_i2c::{I2cDesignwareDriverConfig};
+use kernel::{
+    bindings,
+    module_platform_driver, 
+    of, 
+    platform, 
+    prelude::*,
+    device,
+    device::Device,
+    i2c,
+    i2c::*,
+    c_str,
+    sync::{Arc, ArcBorrow},
+};
 
 module_platform_driver! {
       type: DwI2cDriver,
@@ -28,19 +36,51 @@ kernel::define_of_id_table! {DW_I2C_OF_MATCH_TABLE, (), [
     (of::DeviceId::Compatible(b"snps,designware-i2c"),None),
 ]}
 
-/*
-struct DwI2cDriver {
-    pure_driver: I2cDesignwareDriver,
-    device: Device
-}
-*/
+static mut I2C_DW_ALGO: bindings::i2c_algorithm =  bindings::i2c_algorithm { 
+        master_xfer: None,
+        master_xfer_atomic: None,
+        smbus_xfer: None,
+        smbus_xfer_atomic: None,
+        functionality: None,
+        reg_slave: None,
+        unreg_slave: None,
+};
+
+static I2C_DW_QUIRKS: bindings::i2c_adapter_quirks =  bindings::i2c_adapter_quirks { 
+        flags: I2C_AQ_NO_ZERO_LEN,
+        max_num_msgs: 0,
+        max_write_len: 0,
+        max_read_len: 0,
+        max_comb_1st_msg_len: 0,
+        max_comb_2nd_msg_len: 0,
+};
+
 struct DwI2cDriver;
 
+type DeviceData = device::Data<i2c::Registration<DwI2cDriver>, I2cDwMasterDriver, ()>;
+
+#[vtable]
+impl i2c::I2cAlgo for DwI2cDriver {
+    type Data = Arc<DeviceData>;
+
+    fn master_xfer(_data: ArcBorrow<'_, DeviceData>, _msgs: I2cMsg) -> Result {
+        pr_info!("=============== enter master_xfer");
+        Ok(())
+    }
+
+    fn functionality(_data: ArcBorrow<'_,DeviceData>) -> u32 {
+        pr_info!("=============== enter functionality");
+        0
+    }
+}
+
 impl platform::Driver for DwI2cDriver {
+    type Data = Arc<DeviceData>;
+
     // Linux Raw id table
     kernel::driver_of_id_table!(DW_I2C_OF_MATCH_TABLE);
 
-    fn probe(pdev: &mut platform::Device, _id_info: Option<&Self::IdInfo>) -> Result {
+    fn probe(pdev: &mut platform::Device, _id_info: Option<&Self::IdInfo>) -> Result<Arc<DeviceData>> {
         let irq = pdev.irq_resource(0)?;
         let reg_base = pdev.ioremap_resource(0)?;
         let dev = Device::from_dev(pdev);
@@ -48,15 +88,39 @@ impl platform::Driver for DwI2cDriver {
 
         if i2c_detect_slave_mode(&dev) {
             pr_err!("unimplement dw slave driver");
-            return Ok(());
-        } 
-        
-        let driver_config = I2cDwDriverConfig::new(irq, timing);
+            return Err(ENODEV);
+        }
+
+        // clk
+        let clk = dev.devm_clk_get_default_optional()?;
+        clk.prepare_enable()?;
+        let clk_rate_khz = (clk.get_rate() / 1000 ) as u32;
+
+        // create master driver instance
+        let driver_config = I2cDwDriverConfig::new(irq, timing, clk_rate_khz);
         let mut i2c_master_driver = I2cDwMasterDriver::new(driver_config, reg_base);
-        i2c_master_driver.config_init()?;
         i2c_master_driver.setup()?;
-        //let bus_freq = device::
-        //let i2c_config  = I2cDesignwareDriverConfig::new();
-        Ok(())
+        
+        let data = kernel::new_device_data!(
+            i2c::Registration::<DwI2cDriver>::new(),i2c_master_driver,(),
+            "i2c_dw::Registrations"
+        )?;
+
+        // SAFETY: General part of the data is pinned when `data` is. 
+        // From Pin(&mut <UniqueArc<Data>) get  Pin(&mut <Data>)
+        let data = Arc::<DeviceData>::from(data);
+
+        i2c::Registration::register(
+            data.registrations().ok_or(ENXIO)?.as_pinned_mut(),
+            c_str!("Synopsys DesignWare I2C adapter"), 
+            unsafe {&mut I2C_DW_ALGO},
+            &I2C_DW_QUIRKS,
+            &dev,
+            pdev.of_node(),
+            data.clone(),
+            &THIS_MODULE,
+        )?;
+
+        Ok(data)
     }
 }
