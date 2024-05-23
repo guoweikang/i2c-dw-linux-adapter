439f7a813d7305a9188bcc4a2034db3f30cae51d
guoweikang <guoweikang.kernel@gmail.com>
2024-05-23 16:11:28 +0800

    update: def DwI2cData DwI2cAlgo DwI2cIrqHandler
    --------
    
    Signed-off-by: guoweikang <guoweikang.kernel@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,14 @@
 #![no_std]
 
 mod core_base;
-
 pub(crate) use dw_apb_i2c::{I2cDwDriverConfig, I2cDwMasterDriver};
 pub(crate) use i2c_common::{msg, I2cSpeedMode, I2cTiming};
 
 use kernel::{
-    bindings, c_str, device,
-    device::Device,
-    i2c,
+    irq,
+    driver,
+    bindings, c_str,
+    device::{Device, RawDevice},
     i2c::*,
     module_platform_driver, of, platform,
     prelude::*,
@@ -23,6 +23,7 @@ module_platform_driver! {
       type: DwI2cDriver,
       name: "i2c_designware",
       license: "GPL",
+      initcall: "subsys",
 }
 
 // Linux Raw id table
@@ -51,42 +52,78 @@ static I2C_DW_QUIRKS: bindings::i2c_adapter_quirks = bindings::i2c_adapter_quirk
     max_comb_2nd_msg_len: 0,
 };
 
-struct DwI2cDriver;
-type DeviceData = device::Data<i2c::Registration<DwI2cDriver>, I2cDwMasterDriver, ()>;
+#[pin_data]
+struct DwI2cData {
+    #[pin]
+    i2c_adapter: I2cAdapter<DwI2cAlgo>,
+    #[pin]
+    driver: I2cDwMasterDriver,
+    irq: irq::Registration::<DwI2cIrqHandler>,
+}
 
+impl driver::DeviceRemoval for DwI2cData {
+    fn device_remove(&self) {
+        pr_info!("unimplement DwI2cData Remove");
+    }
+}
+
+impl DwI2cData {
+    fn new(driver: I2cDwMasterDriver,
+        parent: *mut bindings::device,
+        of_node: *mut bindings::device_node,
+        irq: u32,
+    ) -> Arc<Self> {
+        Arc::pin_init(pin_init!(&this in Self {
+            i2c_adapter <- I2cAdapter::<DwI2cAlgo>::new(
+                c_str!("Synopsys DesignWare I2C adapter"),
+                &THIS_MODULE,
+                parent,
+                &I2C_DW_QUIRKS as *const bindings::i2c_adapter_quirks,
+                //SAFETY: init by adapter
+                unsafe {&mut I2C_DW_ALGO as *mut bindings::i2c_algorithm},
+                of_node), 
+            driver: driver,
+            irq: irq::Registration::<DwI2cIrqHandler>::try_new(irq,
+                //SAFETY: this is always valid
+                unsafe {Arc::from_raw(this.as_ptr())},
+                irq::flags::SHARED|irq::flags::COND_SUSPEND,
+                fmt!("dw_i2c_irq_{irq}")).unwrap(),
+        })).unwrap()
+    }
+}
+
+struct DwI2cAlgo;
 #[vtable]
-impl i2c::I2cAlgo for DwI2cDriver {
-    type Data = Arc<DeviceData>;
-    fn master_xfer(_data: ArcBorrow<'_, DeviceData>, msgs: &I2cMsg, msg_num: usize) -> Result {
-        use core::slice;
-        pr_info!("=============== enter master_xfer");
+impl I2cAlgo for DwI2cAlgo {
+    type Data = Arc<DwI2cData>;
+    fn master_xfer(data: ArcBorrow<'_, DwI2cData>, msgs: &I2cMsg, msg_num: usize) -> Result<i32> {
         let trans_msgs = msgs.into_array(msg_num, |x: &mut bindings::i2c_msg| {
-            msg::I2cMsg::new(
-                x.addr,
-                msg::I2cMsgFlags::from_bits(x.flags).unwrap(),
-                unsafe { slice::from_raw_parts(x.buf, x.len as usize) },
-            )
+            msg::I2cMsg::new(x.addr, msg::I2cMsgFlags::from_bits(x.flags).unwrap(), x.buf, x.len as usize)
         })?;
 
-        for m in trans_msgs {
-            pr_info!("================ send msg : {:?}", m);
-        }
-
-        Ok(())
+        let master_driver = &data.driver;
+        master_driver.master_transfer(trans_msgs)
     }
 
-    fn functionality(data: ArcBorrow<'_, DeviceData>) -> u32 {
-        let master_driver = data.resources().unwrap();
-        pr_info!(
-            "======= get functionality {:?}",
-            master_driver.get_functionality().bits()
-        );
+    fn functionality(data: ArcBorrow<'_, DwI2cData>) -> u32 {
+        let master_driver = &data.driver;
         master_driver.get_functionality().bits()
     }
 }
 
+struct DwI2cIrqHandler;
+impl irq::Handler for DwI2cIrqHandler {
+    type Data = Arc<DwI2cData>;
+
+    fn handle_irq(data: ArcBorrow<'_, DwI2cData>) -> irq::Return {
+        let master_driver = &data.driver;
+        master_driver.irq_handler()
+    }
+}
+
+struct DwI2cDriver;
 impl platform::Driver for DwI2cDriver {
-    type Data = Arc<DeviceData>;
+    type Data = Arc<DwI2cData>;
 
     // Linux Raw id table
     kernel::driver_of_id_table!(DW_I2C_OF_MATCH_TABLE);
@@ -94,7 +131,7 @@ impl platform::Driver for DwI2cDriver {
     fn probe(
         pdev: &mut platform::Device,
         _id_info: Option<&Self::IdInfo>,
-    ) -> Result<Arc<DeviceData>> {
+    ) -> Result<Arc<DwI2cData>> {
         let irq = pdev.irq_resource(0)?;
         let reg_base = pdev.ioremap_resource(0)?;
         let dev = Device::from_dev(pdev);
@@ -111,32 +148,19 @@ impl platform::Driver for DwI2cDriver {
         let clk_rate_khz = (clk.get_rate() / 1000) as u32;
 
         // create master driver instance
-        let driver_config = I2cDwDriverConfig::new(irq, timing, clk_rate_khz);
+        let driver_config = I2cDwDriverConfig::new(timing, clk_rate_khz);
         let mut i2c_master_driver = I2cDwMasterDriver::new(driver_config, reg_base);
         i2c_master_driver.setup()?;
 
-        let data = kernel::new_device_data!(
-            i2c::Registration::<DwI2cDriver>::new(),
-            i2c_master_driver,
-            (),
-            "i2c_dw::Registrations"
-        )?;
-
-        // SAFETY: General part of the data is pinned when `data` is.
-        // From Pin(&mut <UniqueArc<Data>) get  Pin(&mut <Data>)
-        let data = Arc::<DeviceData>::from(data);
-
-        i2c::Registration::register(
-            data.registrations().ok_or(ENXIO)?.as_pinned_mut(),
-            c_str!("Synopsys DesignWare I2C adapter"),
-            unsafe { &mut I2C_DW_ALGO },
-            &I2C_DW_QUIRKS,
-            &dev,
-            pdev.of_node(),
-            data.clone(),
-            &THIS_MODULE,
-        )?;
-
+        // create data
+        let data = DwI2cData::new(i2c_master_driver, 
+            dev.raw_device(), 
+            pdev.of_node(), 
+            irq as u32,
+        );
+        
+        //register adapter
+        (&(data.i2c_adapter)).add_numbered_adapter(data.clone())?;
         Ok(data)
     }
 }
